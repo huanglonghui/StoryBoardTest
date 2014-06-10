ebb7e4665d2aa5ee487af0b0a8f7fc3ad6670b12
黄龙辉 <ghuanglonghui@gmail.com>
2014-06-11 06:31:27 +0800

    Initial Commit

---
+++ StoryBoardTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryBoardTest
+//
+//  Created by 黄龙辉 on 14/6/11.
+//  Copyright (c) 2014年 黄龙辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryBoardTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryBoardTest
+//
+//  Created by 黄龙辉 on 14/6/11.
+//  Copyright (c) 2014年 黄龙辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
